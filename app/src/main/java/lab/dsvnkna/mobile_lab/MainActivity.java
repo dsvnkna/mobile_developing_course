8850849df71e1f1563930f49c8d116210e84639a
dsvnkna <dsavinkina@gmail.com>
2018-09-24 01:06:14 +0300

    2 lab
    DoD:
    - Your app has main screen with following items:
    	1) EditText on Android or UITextField on iOS with "Input your name" placeholder. Edit Text supports quick clearing ("clear" button)
    	2) Below - Button on Android or UIButton on IOS with "Say Hello!" label
    	3) Below - UILabel on iOS or TextView on Android that has "Hello #{your_name}" text on Button click
    - All mentioned views are aligned horizontally and vertically
    - Text field supports "normal" and "focused" states
    - Button supports "normal", "selected", "highlighted" states

--- app/src/main/java/lab/dsvnkna/mobile_lab/MainActivity.java
+++ app/src/main/java/lab/dsvnkna/mobile_lab/MainActivity.java
@@ -1,5 +1,6 @@
 package lab.dsvnkna.mobile_lab;
 
+import android.annotation.SuppressLint;
 import android.os.Bundle;
 import android.support.design.widget.FloatingActionButton;
 import android.support.design.widget.Snackbar;
@@ -8,6 +9,9 @@ import android.support.v7.widget.Toolbar;
 import android.view.View;
 import android.view.Menu;
 import android.view.MenuItem;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.TextView;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -15,7 +19,35 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-        Toolbar toolbar = findViewById(R.id.toolbar);
+        final EditText inputName = findViewById(R.id.input_name);
+        final Button clearButton = findViewById(R.id.clear_button);
+        final Button sayHelloButton = findViewById(R.id.hello_button);
+        final TextView sayHelloText = findViewById(R.id.hello_my_name);
+        final Toolbar toolbar = findViewById(R.id.toolbar);
+        sayHelloText.setText(R.string.default_text);
         setSupportActionBar(toolbar);
+
+
+        clearButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                inputName.setText("");
+                sayHelloText.setText(R.string.default_text);
+            }
+        });
+
+        sayHelloButton.setOnClickListener(new View.OnClickListener() {
+            @SuppressLint("SetTextI18n")
+            @Override
+            public void onClick(View view) {
+                if (inputName.getText().toString().equals("")) {
+                    Snackbar.make(view, R.string.empty_name_message, Snackbar.LENGTH_SHORT)
+                            .setAction("Action", null).show();
+                } else {
+                    sayHelloText.setText("Hello " + inputName.getText() + "!");
+                }
+            }
+        });
+
     }
 }
